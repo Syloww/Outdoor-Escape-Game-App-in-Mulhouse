60dd409d2aa008e3e1df53856722b8f86ec8369b
sylow <glenacsalim@gmail.com>
2025-04-14 12:54:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 	Pour mieux comprendre ce script, voir : https://css-tricks.com/serviceworker-for-offline/
 *******************/
 
-var version = 'v1:0:21';
+var version = 'v1:0:22';
 
 self.addEventListener("install", function(event) {
 	self.skipWaiting();
