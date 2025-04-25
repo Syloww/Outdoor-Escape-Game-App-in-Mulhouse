e95d97a1a67cd7da83b5f93bdeb3f62bf5beb3e2
sylow <glenacsalim@gmail.com>
2025-04-25 08:58:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,69 +1,69 @@
 var version = 'v1:0:26';
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
 	self.skipWaiting();
 	event.waitUntil(
-	caches.open(version + 'fundamentals')
-      .then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'manifest.json',
-		  'salim.html'
-        ]);
-      })
+		caches.open(version + 'fundamentals')
+			.then(function (cache) {
+				return cache.addAll([
+					'/',
+					'/index.html',
+					'/manifest.json',
+					'/salim.html'
+				]);
+			})
 	);
 });
 
-self.addEventListener("fetch", function(event) {
-  if (event.request.url.indexOf('http') === 0 && event.request.method == 'GET') {
-	  event.respondWith(
-		caches
-		  .match(event.request)
-		  .then(function(cached) {
-			var networked = fetch(event.request)
-			  .then(fetchedFromNetwork, unableToResolve)
-			  .catch(unableToResolve);
-			return cached || networked;
+self.addEventListener("fetch", function (event) {
+	if (event.request.url.indexOf('http') === 0 && event.request.method == 'GET') {
+		event.respondWith(
+			caches
+				.match(event.request)
+				.then(function (cached) {
+					var networked = fetch(event.request)
+						.then(fetchedFromNetwork, unableToResolve)
+						.catch(unableToResolve);
+					return cached || networked;
 
-			function fetchedFromNetwork(response) {
-			  var cacheCopy = response.clone();
-			  caches.open(version + 'pages')
-					.then(function add(cache) {
-						cache.put(event.request, cacheCopy);
-					});
-			  return response;
-			}
+					function fetchedFromNetwork(response) {
+						var cacheCopy = response.clone();
+						caches.open(version + 'pages')
+							.then(function add(cache) {
+								cache.put(event.request, cacheCopy);
+							});
+						return response;
+					}
 
-			function unableToResolve () {
-			 
-			  return new Response("<h1>Cette ressource n'est pas disponible hors ligne</h1>", {
-				status: 503,
-				statusText: 'Service Unavailable',
-				headers: new Headers({
-				  'Content-Type': 'text/html'
+					function unableToResolve() {
+
+						return new Response("<h1>Cette ressource n'est pas disponible hors ligne</h1>", {
+							status: 503,
+							statusText: 'Service Unavailable',
+							headers: new Headers({
+								'Content-Type': 'text/html'
+							})
+						});
+					}
 				})
-			  });
-			}
-		  })
-	  );
-  }
+		);
+	}
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches
-      .keys()
-      .then(function (keys) {
-        return Promise.all(
-          keys
-            .filter(function (key) {
-              return !key.startsWith(version);
-            })
-            .map(function (key) {
-              return caches.delete(key);
-            })
-        );
-      })
-  );
+self.addEventListener("activate", function (event) {
+	event.waitUntil(
+		caches
+			.keys()
+			.then(function (keys) {
+				return Promise.all(
+					keys
+						.filter(function (key) {
+							return !key.startsWith(version);
+						})
+						.map(function (key) {
+							return caches.delete(key);
+						})
+				);
+			})
+	);
 });
